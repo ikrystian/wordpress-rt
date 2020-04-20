1fe67347db8841e6dc132fdc3f07de23f4e777d9
Krystian <krystian@bpcoders.pl>
2020-04-20 18:23:25 +0200

    Hover menu effect

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,15 +1,18 @@
-$( window ).on( 'load', () => {
+$( window ).on( 'load', function() {
 	$( '#toggle-nav' ).on( 'click', function() {
 		//!todo zmiana tekstu przycisku
 		$( '.nav-container' ).toggleClass( 'nav-container--opened' );
 	} );
 
 	$( '.opening-hours__button' ).on( 'click', function() {
-		console.log( 'asd' );
 		$( '.opening-hours__content' ).toggleClass( 'opening-hours__content--opened' );
 	} );
-
+	let menuLinks = $( '.menu-main-container a' );
+	$( menuLinks ).on( 'mouseenter', function() {
+		menuLinks.not( $( this ) ).addClass( 'not-active' );
+	} ).on( 'mouseleave', function() {
+		menuLinks.removeClass( 'not-active' );
+	} );
 } );
-
 // zrobimy refactor do czystego js
 
